0c05f87caba4fdfc9e8946e5f12c412b1b3fc973
RuixiangJiang <2037358823@qq.com>
2025-09-03 11:13:54 -0400

    upd base dir

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/vue-resume-template/',
+    base: '/',
     plugins: [vue()],
     css: {
         preprocessorOptions: {
